b29a76a0e52c2af13ba5d43563575c29d22f8a01
jeffersbaxter <jeffersbaxter@comcast.net>
2022-09-16 14:59:55 -0700

    write high score to file and retrieve between games

--- scoreboard.py
+++ scoreboard.py
@@ -11,18 +11,26 @@ class Scoreboard(Turtle):
         self.pu()
         self.color("white")
         self.goto(0, 270)
-        self.points = 0
+        self.score = 0
+        with open('data.txt') as file:
+            self.high_score = int(file.read())
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.points}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+
+            with open('data.txt', mode="w") as file:
+                file.write(str(self.high_score))
+
+        self.score = 0
+        self.update_scoreboard()
 
     def add_point(self):
-        self.points += 1
-        self.clear()
+        self.score += 1
         self.update_scoreboard()
